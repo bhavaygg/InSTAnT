5bfa515a568bd5c4b2863b72b9e10714b7d24107
Bhavay Aggarwal <baggarwal9@login-phoenix-slurm-3.pace.gatech.edu>
2024-01-15 18:14:56 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+from distutils.core import setup
+setup(
+  name = 'InSTAnT',         
+  packages = ['InSTAnT'],   
+  version = '1.0',      
+  license='MIT',        
+  description = 'InSTAnT is a toolkit to identify gene pairs which are d-colocalized from single molecule measurement data.',   
+  author = 'Anurendra Kumar, Bhavay Aggarwal',                   
+  author_email = 'anu.ankesh@gmail.com, bhavayaggarwal07@gmail.com',      
+  url = 'https://github.com/anurendra, https://github.com/Chokerino',   
+  keywords = ['InSTAnT', 'Python'],   
+  install_requires=[            
+          'numpy',
+          'scipy',
+          'pandas',
+          'pickle',
+          'scikit-learn',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

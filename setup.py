c83e03823ef8e7edc8526d328e76c88497c2c747
Bhavay Aggarwal <bagg7@gatech.edu>
2024-04-18 15:24:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     package_dir={
         'InSTAnT': 'InSTAnT',
     },
-  version = '1.2',      
+  version = '1.2.1',      
   license='MIT',        
   description = 'InSTAnT is a toolkit to identify gene pairs which are d-colocalized from single molecule measurement data.',   
   author = 'Anurendra Kumar, Bhavay Aggarwal',                   
